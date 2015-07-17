e1661a67b9b9bd6f594b466950075cd99d3a5efb
Takeshi YAMAMURO <linguin.m.s@gmail.com>
2015-07-17 17:36:30 +0900

    Add codes to load libnuma from native

--- src/main/java/xerial/jnuma/JnumaLibLoader.java
+++ src/main/java/xerial/jnuma/JnumaLibLoader.java
@@ -18,23 +18,48 @@ package xerial.jnuma;
 
 import xerial.jnuma.utils.NativeLibLoader;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Load a native library into JVM.
  */
 public class JnumaLibLoader {
+  private static final Set<String> LIBNUMA_PATHS;
+
+  static {
+    LIBNUMA_PATHS = new HashSet<String>();
+    LIBNUMA_PATHS.add(System.getProperty("xerial.junma.libnuma.path"));
+    LIBNUMA_PATHS.add(System.getenv("NUMALIB_LIBRARY_PATH"));
+    LIBNUMA_PATHS.add("/usr/lib/x86_64-linux-gnu/libnuma.so");
+  }
+
   private static NativeLibLoader loader = new NativeLibLoader("jnuma");
+  private static boolean isLibNumaLoaded = false;
   private static boolean isLoaded = false;
 
   static synchronized public void load() throws Exception {
     if (isLoaded) return;
     // Load a library for libnuma in advance
-    // TODO: Remove a hard code for lading libnuma
-    try {
-      System.load("/usr/lib/x86_64-linux-gnu/libnuma.so");
-    } catch (Error e) {
-      throw new RuntimeException();
+    // TODO: Why System.loadLibrary("numa") does not work correctly?
+    for (String libPath : LIBNUMA_PATHS) {
+      if (tryLoad(libPath)) {
+        isLibNumaLoaded  = true;
+        break;
+      }
     }
+    if (!isLibNumaLoaded)
+      throw new RuntimeException();
     loader.load();
     isLoaded = true;
+  }
+
+  static boolean tryLoad(String name) {
+    try {
+      System.load(name);
+    } catch (Throwable e) {
+      return false;
+    }
+    return true;
   }
 }
