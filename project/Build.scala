564eb42d6e31025d9a779534dc8b26626d8d6d7c
Takeshi YAMAMURO <linguin.m.s@gmail.com>
2015-07-19 00:15:00 +0900

    Upgrade v0.1.3 to v0.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends sbt.Build {
     settings = Project.defaultSettings ++ Seq(
       name := "jnuma",
       organization := "xerial",
-      version := "0.1.3",
+      version := "0.2.0",
       scalaVersion := "2.10.4"
       // add other settings here
     )
